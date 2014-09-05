f368ea1a42da4c3f087ae9a728dc034e058351c9
shibafu <shibafu528@gmail.com>
2014-09-05 18:53:41 +0900

    Initial Commit

---
+++ NotepadManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace NoteTweetConnector
+{
+	public class NotepadManager
+	{
+		[DllImport("user32.dll")]
+		private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 msg, IntPtr wp, byte[] lp);
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr FindWindowEx(IntPtr hWnd, IntPtr hwndChildAfter, string lpszChass, string lpszWindow);
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+		public const uint WM_SETTEXT = 0x000C;
+		public const int GWL_HINSTANCE = (-6);
+
+		private IntPtr hInstance;
+		private IntPtr hWndNotepad;
+		private IntPtr hWndEdit;
+
+		public NotepadManager()
+		{
+			hWndNotepad = FindWindow("NotePad", null);
+			if (hWndNotepad != IntPtr.Zero)
+			{
+				hWndEdit = FindWindowEx(hWndNotepad, IntPtr.Zero, "Edit", null);
+				hInstance = GetWindowLong(hWndNotepad, GWL_HINSTANCE);
+			}
+			else
+				throw new InvalidOperationException();
+		}
+
+		public void setText(string text)
+		{
+			SendMessage(hWndEdit, WM_SETTEXT, IntPtr.Zero, Encoding.GetEncoding(932).GetBytes(text));
+		}
+
+		public void hookWindow()
+		{
+		}
+
+		public void unHookWindow()
+		{
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NoteTweetConnector
+{
+	class MainClass
+	{
+		private const String CONSUMER_KEY = "E8DeKvJHpAlCEGa9hI657tME5";
+		private const String CONSUMER_SECRET = "tieRk1ccVu4PoWUXLOG6IFKTQ3GYSXZpzvcRq2Z9uG3nBYoeU7";
+
+		public static void Main(string[] args)
+		{
+			var notepad = new NotepadManager();
+
+			var tokens = CoreTweet.Tokens.Create(CONSUMER_KEY, CONSUMER_SECRET, "26197127-sBh53zMbsYqHl4vKzZE81UOlQwhUGTw8VswkfhsCA", "FIBqQp99Whnee2MX4EoQXE7UxE8964x9ULVwRb6WYXFSY");
+
+			var textBuffer = "";
+			foreach (var status in tokens.Statuses.HomeTimeline())
+			{
+				if (status.User.IsProtected)
+					continue;
+				textBuffer += formatTweet(status) + "\n";
+			}
+			notepad.setText(textBuffer);
+
+			var stream = tokens.Streaming.StartStream(CoreTweet.Streaming.StreamingType.User);
+			foreach (var message in stream)
+			{
+				if (message is CoreTweet.Streaming.StatusMessage)
+				{
+					var status = (message as CoreTweet.Streaming.StatusMessage).Status;
+					if (!status.User.IsProtected)
+						textBuffer = formatTweet(status) + "\n" + textBuffer;
+				}
+				notepad.setText(textBuffer);
+			}
+		}
+
+		private static String formatTweet(CoreTweet.Status status)
+		{
+			return string.Format("[{0}] {1}: {2}", status.CreatedAt.ToLocalTime(), status.User.ScreenName, status.Text);
+		}
+	}
+}
